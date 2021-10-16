9a35577974a801d5dbd2a69c0b3a0e2f9ace09f1
rafaelmoraiscunha <rafael.morais@franq.com.br>
2021-10-16 19:39:45 -0300

    [FEATURE] envio de dados comunicando com front end

--- src/handlers/sendMail.js
+++ src/handlers/sendMail.js
@@ -1,17 +1,43 @@
+import AWS from "aws-sdk"
+const ses = new AWS.SES({ region: "sa-east-1" })
+
 async function sendMail(event, context) {
 
   const data = JSON.parse(event.body)
 
-  const headers = {
-    'Access-Control-Allow-Origin': '*',
-    'Access-Control-Allow-Credentials': true,
+  const body = `A pessoa ${data.message.nome} do telefone ${data.message.telefone} pediu o item ${data.message.id}`
+  const params = {
+    Source: "rafaelcmorais02@gmail.com",
+    Destination: {
+      ToAddresses: ["emillie.sara1@hotmail.com"]
+    },
+    Message: {
+      Body: {
+        Text: {
+          Data: body
+        }
+
+      },
+      Subject: {
+        Data: "Interesse de compra"
+      },
+    }
   }
 
-  return {
-    statusCode: 200,
-    headers,
-    body: JSON.stringify(data),
-  };
+  try {
+    await ses.sendEmail(params).promise()
+    const headers = {
+      'Access-Control-Allow-Origin': '*',
+      'Access-Control-Allow-Credentials': true,
+    }
+    return {
+      statusCode: 200,
+      headers,
+      body: JSON.stringify(data),
+    };
+  } catch (error) {
+    console.error(error)
+  }
 }
 
 export const handler = sendMail;
