10e2194383734d1f6531ce031b167cf0d941afbc
rafaelmoraiscunha <rafael.morais@franq.com.br>
2021-10-18 22:48:24 -0300

    [FEATURE] melhoria da integraçao do backend com frontend

--- src/handlers/sendMail.js
+++ src/handlers/sendMail.js
@@ -5,38 +5,52 @@ async function sendMail(event, context) {
 
   const data = JSON.parse(event.body)
 
-  const body = `A pessoa ${data.message.nome} do telefone ${data.message.telefone} pediu o item ${data.message.id}`
-  const params = {
-    Source: "levemenina27@gmail.com",
-    Destination: {
-      ToAddresses: ["levemenina27@gmail.com"]
-    },
-    Message: {
-      Body: {
-        Text: {
-          Data: body
-        }
+  const { message: { nome, telefone, id } } = data
 
+  if ((nome) && (telefone)) {
+
+    const body = `A pessoa ${nome} do telefone ${telefone} pediu o item ${id}`
+
+    const params = {
+      Source: "levemenina27@gmail.com",
+      Destination: {
+        ToAddresses: ["levemenina27@gmail.com", "rafaelcmorais02@gmail.com"]
       },
-      Subject: {
-        Data: "Interesse de compra"
-      },
+      Message: {
+        Body: {
+          Text: {
+            Data: body
+          }
+
+        },
+        Subject: {
+          Data: "Interesse de compra"
+        },
+      }
+    }
+    try {
+      await ses.sendEmail(params).promise()
+      return {
+        statusCode: 200,
+        headers: {
+          'Access-Control-Allow-Origin': '*',
+          'Access-Control-Allow-Credentials': true,
+        },
+        body: JSON.stringify(data),
+      };
+    } catch (error) {
+      console.error(error)
     }
   }
-
-  try {
-    await ses.sendEmail(params).promise()
-    const headers = {
-      'Access-Control-Allow-Origin': '*',
-      'Access-Control-Allow-Credentials': true,
-    }
+  else {
     return {
-      statusCode: 200,
-      headers,
-      body: JSON.stringify(data),
-    };
-  } catch (error) {
-    console.error(error)
+      statusCode: 400,
+      headers: {
+        'Access-Control-Allow-Origin': '*',
+        'Access-Control-Allow-Credentials': true,
+      },
+      body: JSON.stringify({ message: "todos os campos precisam ser completados" }),
+    }
   }
 }
 
